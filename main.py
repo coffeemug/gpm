729033f9f2c7d6f8e043a74d3f6e67bae5c9515d
Slava Akhmechet <coffeemug@gmail.com>
2014-01-07 22:44:34 -0800

    delight

--- main.py
+++ main.py
@@ -6,91 +6,8 @@ import requests
 import json
 import argparse
 
-USER=None
-REPO=None
-TOKEN=None
-ISSUES=None
-
-def cmd_init():
-    # Too many arguments
-    if len(sys.argv) > 4:
-        print 'Unknown argument to `init`'
-        print_init_help()
-        return
-    # Too few arguments
-    if len(sys.argv) < 3:
-        print 'Too few arguments to `init`'
-        print_init_help()
-        return
-    try:
-        os.makedirs('.p')
-        os.chdir('.p')
-        with open('repo', 'w') as repo:
-            repo.write(sys.argv[2])
-            repo.write('\n')
-        with open('issue_cache', 'w') as issue_cache:
-            issue_cache.write(json.dumps({}))
-        with open('token', 'w') as token:
-            if len(sys.argv) == 4:
-                token.write(sys.argv[3])
-        with open('.gitignore', 'w') as ignore:
-            ignore.write('token\n')
-            ignore.write('issue_cache\n')
-    except:
-        print "Can't initialize the priority queue. Does one already exist?"
-        return
-
-    print """Successfully initialized the priority queue. Now run `pm fetch` to
-fetch latest issues"""
-
-def setup_env(last_dir=None):
-    if last_dir == os.getcwd():
-        print "Can't find priority queue"
-        return
-    if os.path.isdir('.p'):
-        os.chdir('.p')
-        with open('repo', 'r') as repo:
-            global REPO
-            REPO = repo.readline().strip()
-        with open('issue_cache', 'r') as issue_cache:
-            global ISSUES
-            ISSUES = {}
-            issues = json.loads(issue_cache.read())
-            for issue in issues:
-                ISSUES[int(issue)] = issues[issue]
-        with open('token', 'r') as token:
-            global TOKEN
-            TOKEN = token.readline().strip()
-    else:
-        cwd = os.getcwd()
-        os.chdir('..')
-        setup_env(last_dir=cwd)
-
-def cmd_fetch():
-    setup_env()
-    global REPO
-    global ISSUES
-    global TOKEN
-    url = 'https://api.github.com/repos/' + REPO + '/issues?state=open'
-    new_issues = {}
-    while url:
-        if TOKEN:
-            r = requests.get(url, auth=(TOKEN, 'x-oauth-basic'))
-        else:
-            r = requests.get(url)
-        for issue in r.json():
-            new_issues[issue['number']] = issue['title']
-        if 'next' in r.links:
-            url = r.links['next']['url']
-        else:
-            url = None
-    count = len(new_issues)
-    new_count = len(set(new_issues) - set(ISSUES))
-    closed_count = len(set(ISSUES) - set(new_issues))
-    ISSUES = new_issues
-    with open('issue_cache', 'w') as issue_cache:
-        issue_cache.write(json.dumps(ISSUES))
-    print 'Successfully fetched ' + str(count) + ' issues, ' + str(new_count) + ' updated, ' + str(closed_count) + ' closed'
+CONFIG_DIR_PATH = os.path.join(os.path.expanduser('~'), '.gpm')
+CONFIG_FILE_PATH = os.path.join(CONFIG_DIR_PATH, 'config')
 
 def init_arg_parser():
     parser = argparse.ArgumentParser(description='Query github issues.')
@@ -102,16 +19,116 @@ def init_arg_parser():
     parser.add_argument('-f', '--fetch', action='store_true', help='fetch updates from github')
     parser.add_argument('-b', '--browser', action='store_true', help='open issue list in the browser')
     ## Filters
-    parser.add_argument('-s', '--status', choices=['open', 'closed', 'all'], default='open', help='which issues to show (defaults to open)')
     parser.add_argument('-o', '--owner', action='append', help='the owner of the issue; pass multiple for a disjunction')
     parser.add_argument('-m', '--milestone', action='append', help='the milestone of the issue; pass multiple for a disjunction')
     #parser.add_argument('-l', '--labels', nargs='+', action='append', help='the labels on the issue; pass list for a conjunction; pass multiple for a disjunction')
 
     return parser
 
+def save_config(config):
+    if not os.path.isdir(CONFIG_DIR_PATH):
+        os.makedirs(CONFIG_DIR_PATH)
+    with open(CONFIG_FILE_PATH, 'w') as config_file:
+        config_file.write(json.dumps(config))
+    return config
+
+def load_config():
+    try:
+        with open(CONFIG_FILE_PATH, 'r') as config_file:
+            return json.loads(config_file.read())
+    except IOError:
+        config = {}
+        save_config(config)
+        return config
+
+def cache_args_in_config(config, args):
+    if args['token']:
+        config['token'] = args['token']
+    if args['user']:
+        config['user'] = args['user']
+    if args['repo']:
+        config['repo'] = args['repo']
+    return config
+
+def cache_issues(config, issues):
+    cache_dir = os.path.join(CONFIG_DIR_PATH, config['user'])
+    if not os.path.isdir(cache_dir):
+        os.makedirs(cache_dir)
+    with open(os.path.join(cache_dir, config['repo']), 'w') as issue_cache:
+        issue_cache.write(json.dumps(issues))
+
+def load_issues(config):
+    cache_file = os.path.join(CONFIG_DIR_PATH, config['user'], config['repo'])
+    try:
+        with open(cache_file, 'r') as issue_cache:
+            return json.loads(issue_cache.read())
+    except IOError:
+        print "No issue cache, run with --fetch"
+        sys.exit(-1)
+
+def fetch_issues(config):
+    print 'Fetching issues...'
+    url = 'https://api.github.com/repos/' + config['user'] + '/' + config['repo'] + '/issues?state=open'
+    issues = {}
+    while url:
+        if 'token' in config:
+            r = requests.get(url, auth=(config['token'], 'x-oauth-basic'))
+        else:
+            r = requests.get(url)
+        result = r.json()
+        if 'message' in result:
+            print result['message']
+            sys.exit(-1)
+        for issue in result:
+            _issue = { 'owner': None, 'milestone': None}
+            _issue['title'] = issue['title']
+            if 'assignee' in issue and issue['assignee']:
+                _issue['owner'] = issue['assignee']['login']
+            if 'milestone' in issue and issue['milestone']:
+                _issue['milestone'] = issue['milestone']['title']
+            issues[issue['number']] = _issue
+        if 'next' in r.links:
+            url = r.links['next']['url']
+        else:
+            url = None
+    print 'Successfully fetched ' + str(len(issues)) + ' issues'
+    cache_issues(config, issues)
+    return issues
+
+def print_issues(issues):
+    for issue in issues:
+        number = issue
+        issue = issues[number]
+        print '* [#' + number + '] ' + str(issue['title'])
+        print '  Owner: ' + str(issue['owner']) + ', Milestone: ' + str(issue['milestone'])
+
+def filter_issues(issues, args):
+    _issues = {}
+    for issue in issues:
+        number = issue
+        issue = issues[number]
+        if args['owner']:
+            if issue['owner'] not in args['owner']:
+                continue
+        if args['milestone']:
+            if issue['milestone'] not in args['milestone']:
+                continue
+        _issues[number] = issue
+    return _issues
+
 def main():
-    args = init_arg_parser().parse_args()
-    print args
+    args = vars(init_arg_parser().parse_args())
+    config = save_config(cache_args_in_config(load_config(), args))
+    if 'user' not in config:
+        print 'Please specify the user/org'
+        sys.exit(-1)
+    if 'repo' not in config:
+        print 'Please specify the repo'
+        sys.exit(-1)
+    if args['fetch']:
+        fetch_issues(config)
+    issues = load_issues(config)
+    print_issues(filter_issues(issues, args))
 
 if __name__ == '__main__':
     main()
